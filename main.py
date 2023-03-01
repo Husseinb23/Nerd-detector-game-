d078789aff832d4784f68ff94119fc45b6a1e030
Hussein Barakat <121978208+Husseinb23@users.noreply.github.com>
2023-03-01 04:52:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,65 @@
+print("Nerd detector 👀🤓👀")  
+MathKid = input("What's your favorite math subject? ")
+if MathKid == "algebra":
+ print("Your a nerd!")
+elif MathKid == "geometry":
+ print("Your a nerd🤓!")
+elif MathKid == "trigonometry":
+ print("Your a nerd!🤓")
+elif MathKid == "calculus":
+ print("Big nerd🤓!")
+elif MathKid == "functions":
+ print("Big nerd!🤓")
+elif MathKid == "none":
+  print("Your not a nerd👏👏👏")
+elif MathKid == "I hate math":
+  print("Your not a nerd👏👏👏")
+else:
+  print("your not a nerd!👏👏👏")
+favScience = input("What your favorite Science Subject")
+if favScience == "physics":
+    print("Sup Einstien!🤓(your a nerd)")
+elif favScience == "chemistry":
+    print("Oh actually- (nerd!)🤓")
+elif favScience =="chem":
+  print("Oh actually- (nerd!)🤓")
+elif favScience == "biology":
+    print("I see you memorizer(your not a nerd👏👏👏")
+elif favScience == "bio":
+  print("I see you memorizer(your not a nerd👏👏👏")
+elif favScience == "none":
+    print("Congrats your not a nerd 👏👏👏")
+else:
+  print("I see your not a nerd👏")
+codeOr = input("Do you code?")
+if codeOr == "yes":
+    print("Computer nerd!(very ironic of me)🤓")
+elif codeOr == "no":
+ print("then why you on replit?😂")
+elif codeOr == "yea":
+  print("Computer nerd!(very ironic of me 🤓")
+else:
+  print("Your not a nerd👏👏👏")
+glassesOrnot = input("Do you wear glasses or not?")
+if glassesOrnot == "yes":
+    print("yk what that means...🤓🤓🤓")
+elif glassesOrnot == "yea":
+ print("yk what that means...🤓🤓🤓")
+elif glassesOrnot == "no":
+   print("Your not a nerd! 👏👏👏")
+else:
+    print("Your not a nerd! 👏👏👏")
+print("If You Got 2/4 Your a nerd's! then welcome to the nerd club 🤓 👊👊👊")
+
+
+    
+  
+  
+  
+    
+  
+  
+    
+  
+
+
